efe76d408b236cc05708dfd50c6613ab2a369041
thenesterov <thenesterov@users.noreply.github.com>
2025-04-13 07:09:17 +0000

    Deploying to gh-pages from @ thenesterov/mrtest@320882644e4a91d0fca1ef920c1b15cc0d6067ef 🚀

--- node.d.ts
+++ node.d.ts
@@ -8388,6 +8388,10 @@ declare namespace $ {
 declare namespace $ {
 
 	export class $mol_chip extends $mol_view {
+		hint( ): string
+		attr( ): ({ 
+			'title': ReturnType< $mol_chip['hint'] >,
+		})  & ReturnType< $mol_view['attr'] >
 		sub( ): readonly(any)[]
 	}
 	
