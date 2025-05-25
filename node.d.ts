7e7e2f0b7aee5a3922cfd38a5a4218841d945970
thenesterov <thenesterov@users.noreply.github.com>
2025-05-25 07:09:38 +0000

    Deploying to gh-pages from @ thenesterov/mrtest@320882644e4a91d0fca1ef920c1b15cc0d6067ef 🚀

--- node.d.ts
+++ node.d.ts
@@ -1174,7 +1174,7 @@ declare namespace $.$$ {
         pages_deep(): $mol_view[];
         title(): string;
         menu_title(): string;
-        sub(): readonly $mol_view[];
+        sub(): $mol_view[];
         bring(): void;
     }
 }
@@ -7154,6 +7154,7 @@ declare namespace $.$$ {
         cell_expanded(id: {
             row: string[];
         }, next?: boolean): boolean;
+        sub(): readonly any[];
     }
 }
 
