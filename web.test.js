a0d9695b32c18d49627c7c380111607ef64ba25e
thenesterov <thenesterov@users.noreply.github.com>
2025-07-04 07:12:38 +0000

    Deploying to gh-pages from @ thenesterov/mrtest@320882644e4a91d0fca1ef920c1b15cc0d6067ef 🚀

--- web.test.js
+++ web.test.js
@@ -1654,8 +1654,6 @@ var $;
                         $mol_wire_sync(this).wait();
                     return this.value();
                 }
-                static test() {
-                }
             }
             __decorate([
                 $mol_wire_solo
@@ -1666,9 +1664,6 @@ var $;
             __decorate([
                 $mol_wire_solo
             ], App, "result", null);
-            __decorate([
-                $mol_wire_method
-            ], App, "test", null);
             $mol_assert_equal(App.result(), 1);
             App.resets(null);
             $mol_wire_fiber.sync();
