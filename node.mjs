afdbddd74c9bf27a8175d72649be1f4dcb9c10e2
thenesterov <thenesterov@users.noreply.github.com>
2025-08-25 07:12:08 +0000

    Deploying to gh-pages from @ thenesterov/mrtest@320882644e4a91d0fca1ef920c1b15cc0d6067ef 🚀

--- node.mjs
+++ node.mjs
@@ -10756,7 +10756,7 @@ var $;
 (function ($) {
     $.$hyoo_crus_vary_mapping = {
         nil: null,
-        bin: Uint8Array,
+        bin: (Uint8Array),
         bool: Boolean,
         int: BigInt,
         real: Number,
