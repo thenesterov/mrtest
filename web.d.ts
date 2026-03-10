1f7456cc75f9875bd4c6499b96ba06a072b91ab5
thenesterov <thenesterov@users.noreply.github.com>
2026-03-10 07:33:06 +0000

    Deploying to gh-pages from @ thenesterov/mrtest@320882644e4a91d0fca1ef920c1b15cc0d6067ef 🚀

--- web.d.ts
+++ web.d.ts
@@ -3508,8 +3508,11 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_crypto_salt(): Uint8Array<ArrayBuffer>;
-    const $mol_crypto_salt_once: Uint8Array<ArrayBuffer>;
+    function $mol_crypto2_nonce(): Uint8Array<ArrayBuffer>;
+}
+
+declare namespace $ {
+    let $mol_crypto_salt: typeof $mol_crypto2_nonce;
 }
 
 declare namespace $ {
@@ -3880,7 +3883,11 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_crypto_hash(input: ArrayBufferView): Uint8Array<ArrayBuffer>;
+    function $mol_crypto2_hash(input: ArrayBufferView): Uint8Array<ArrayBuffer>;
+}
+
+declare namespace $ {
+    let $mol_crypto_hash: typeof $mol_crypto2_hash;
 }
 
 declare namespace $ {
